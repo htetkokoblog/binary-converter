a298ce84450f4f59fa59f4d1fa42d4cbeaac0c2f
Htet Ko Ko <htetkokoemail@gmail.com>
2024-09-27 16:28:32 +0630

    Create main.js

---
+++ data/main.js
@@ -0,0 +1,43 @@
+app.controller('converter',function($scope){
+  $scope.textToBinary = function (text){
+    return text
+      .split('')
+      .map(char => char.charCodeAt(0).toString(2).padStart(8, '0'))
+      .join(' ');
+  };
+  $scope.binaryToText = function (binary){
+    return binary
+    .split(' ')
+    .map(b => String.fromCharCode(parseInt(b, 2)))
+    .join('');
+  };
+  $scope.convert = function(txt){
+    if (/^[01\s]+$/.test(txt.trim())) {
+      // It looks like binary code
+      const convertedText = $scope.binaryToText(txt.trim());
+      $scope.result = convertedText;
+      $scope.convertedMessage = 'Converted Text √';
+    } else {
+      // It's regular text, convert to binary
+      const binaryCode = $scope.textToBinary(txt);
+      $scope.result = binaryCode;
+      $scope.convertedMessage = 'Converted Binary Code √';
+    }
+    if (txt === '') {
+      $scope.convertedMessage = '';
+    }
+  };
+  $scope.copy = function(copyTxt) {
+    navigator.clipboard.writeText(copyTxt)
+      .then(() => {
+        alert(copyTxt);
+      });
+  };
+  $scope.shareTxt = function(txt) {
+    if (navigator.share) {
+      navigator.share({ text: txt });
+    } else {
+      alert('Web Share API is not supported in this browser.');
+    }
+  };
+})
